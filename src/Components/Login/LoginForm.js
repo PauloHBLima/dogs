d9a9cb8677b1f8291eb9189f4de5584f0e73f679
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-04-24 23:33:24 -0300

    useForm, Input

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../Form/Input';
 import Button from '../Form/Button';
+import useForm from '../../Hooks/useForm';
 
 function LoginForm() {
-  const [username, setUsername]  = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm('email');
+  const password = useForm();
 
   function handleLogin(event) {
     event.preventDefault();
+
+    if(username.validade() && password.validade)
     fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({username, password}),
+      body: JSON.stringify({}),
     }).then((response) => {
       console.log(response);
       return response.json();
@@ -27,8 +30,8 @@ function LoginForm() {
     <section>
      <h1>Login</h1>
      <form action='' onSubmit={handleLogin}>
-      <Input label="Usuário" type="text" name="username"/>
-      <Input label="Senha" type="password" name="password"/>
+      <Input label="Usuário" type="text" name="username" {...username}/>
+      <Input label="Senha" type="password" name="password" {...password}/>
       <Button>Entrar</Button>
       
       <button>Entrar</button>
