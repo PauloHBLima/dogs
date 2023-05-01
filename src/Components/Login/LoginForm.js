8fe113abad91309a19c20c2d08f91c12522525a4
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-04-30 22:10:42 -0300

    styles pages login/register + register user

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,6 +4,9 @@ import Input from '../Form/Input';
 import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
 import { UserContext } from '../../UserContext';
+import Error from '../Helper/Error';
+import styles from '../Login/LoginForm.module.css';
+import stylesBtn from '../Form/Button.module.css';
 
 const LoginForm = () => {
   const username = useForm();
@@ -15,23 +18,33 @@ const LoginForm = () => {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      userLogin(username.value, password.value)
+      userLogin(username.value, password.value);
     }
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleLogin}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleLogin}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        {loading ? <Button disable>Carregando ...</Button> 
-        : (
-        <Button>Entrar</Button>
+        {loading ? (
+          <Button disable>Carregando ...</Button>
+        ) : (
+          <Button>Entrar</Button>
         )}
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className={styles.perdeu} to="/login/perdeu">
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastra-se no site</p>
+        <Link className={stylesBtn.button} to="/login/criar">
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 };
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex:
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
+    message: "Precisa possuir no mínimo 8 caracteres, variedade de letras maiúsculas e minúsculas, números e caractere especial."
+  },
 };
 const useForm = (type) => {
   const [value, setValue] = React.useState('');
